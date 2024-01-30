489efbf080b2ecde71323c12048857c9e004a3a6
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-01-30 18:30:29 +0600

    projct done use botstarp framework

--- src/component/Cursol.jsx
+++ src/component/Cursol.jsx
@@ -1,42 +1,77 @@
 import React from 'react'
-
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 import one from '../assets/1.jpg'
 import two from '../assets/2.jpg'
-import thre from '../assets/3.png'
-export const Cursol = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
+import thre from '../assets/3.png';
+
+
+function Arrow(props) {
+  const { className, style, onClick } = props;
   return (
-    <div>
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black" }}
+      onClick={onClick}
+    />
+  );
+}
+
+
+export const Cursol = () => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    nextArrow: <Arrow />,
+    prevArrow: <Arrow />,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      
+    ]
+  };
+  return (
+    <div className='container'>
         <h2> Single Item</h2>
         <Slider {...settings}>
           <div>
-            <img src={one}/>
+            <img src={one}  className='w-50' />
           </div>
           <div>
-          <img src={two}/>
+          <img src={two} className='w-50'/>
           </div>
           <div>
-          <img src={thre}/>
+          <img src={thre} className='w-100'/>
           </div>
           <div>
-          <img src={one}/>
+          <img src={two} className='w-50'/>
           </div>
           <div>
-          <img src={thre}/>
+          <img src={thre} className='w-100'/>
           </div>
           <div>
-          <img src={one}/>
+          <img src={two} className='w-50'/>
           </div>
+         
         </Slider>
       </div>
   )
